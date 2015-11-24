d1cd3f46c3054205a2c148bdee23cb37bf1b5043
n2o <christian.meter@uni-duesseldorf.de>
2015-11-24 15:22:36 +0100

    Adjust styles

--- wiki/views.py
+++ wiki/views.py
@@ -4,4 +4,6 @@ from .models import Item
 
 def list(request):
     items = get_list_or_404(Item)
+    for item in items:
+        print(item.related)
     return render(request, 'wiki/list.html', {'items': items})
